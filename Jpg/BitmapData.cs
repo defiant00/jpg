7a6246835bc985aa1a1ffac8c74c988083f26558
Aaron Mady <defiant00@gmail.com>
2017-07-17 17:38:16 -0400

    properly create a bitmap file

--- Jpg/BitmapData.cs
+++ Jpg/BitmapData.cs
@@ -6,7 +6,7 @@ namespace Jpg
 	{
 		public struct Pixel
 		{
-			byte R, G, B;
+			public byte R, G, B;
 		}
 
 		public int Width, Height;
@@ -31,22 +31,35 @@ namespace Jpg
 			using (var str = new FileStream(fileName, FileMode.Create))
 			using (var writer = new BinaryWriter(str))
 			{
-				writer.Write('B');								// BM indicator
+				int rowSize = ((24 * Width + 31) / 32) * 4;
+
+				writer.Write('B');                              // BM indicator
 				writer.Write('M');
-				writer.Write(14 + 40 + Width * Height * 3);		// File Size: File header, DIB header, pixel data
-				writer.Write(0);								// Reserved
-				writer.Write(54);								// Pixel data starting address
-				writer.Write(40);								// DIB header size
-				writer.Write(Width);							// Image width
-				writer.Write(Height);							// Image height
-				writer.Write((short)1);							// Number of color panes (must be 1)
-				writer.Write((short)24);						// Bits per pixel
-				writer.Write(0);								// Compression method (none)
-				writer.Write(0);								// Image size (unnecessary if no compression method is given)
-				writer.Write(3780);								// Horizontal pixels per meter
-				writer.Write(3780);								// Vertical pixels per meter
-				writer.Write(0);								// Number of colors in the palette
-				writer.Write(0);								// Number of important colors (generally ignored)
+				writer.Write(14 + 40 + rowSize * Height);		// File Size: File header, DIB header, pixel data
+				writer.Write(0);                                // Reserved
+				writer.Write(54);                               // Pixel data starting address
+				writer.Write(40);                               // DIB header size
+				writer.Write(Width);                            // Image width
+				writer.Write(Height);                           // Image height
+				writer.Write((short)1);                         // Number of color panes (must be 1)
+				writer.Write((short)24);                        // Bits per pixel
+				writer.Write(0);                                // Compression method (none)
+				writer.Write(0);                                // Image size (unnecessary if no compression method is given)
+				writer.Write(3780);                             // Horizontal pixels per meter
+				writer.Write(3780);                             // Vertical pixels per meter
+				writer.Write(0);                                // Number of colors in the palette
+				writer.Write(0);                                // Number of important colors (generally ignored)
+				for (int h = Height - 1; h >= 0; h--)
+				{
+					var bArr = new byte[rowSize];
+					for (int w = 0; w < Width; w++)
+					{
+						bArr[w * 3] = Data[w, h].B;
+						bArr[w * 3 + 1] = Data[w, h].G;
+						bArr[w * 3 + 2] = Data[w, h].R;
+					}
+					writer.Write(bArr);
+				}
 			}
 		}
 	}
--- Jpg/MainForm.cs
+++ Jpg/MainForm.cs
@@ -15,12 +15,16 @@ namespace Jpg
 			//var open = new OpenFileDialog { Filter = "JPG Image|*.jpg;*.jpeg" };
 			//if (open.ShowDialog() == DialogResult.OK)
 			//{
-				var save = new SaveFileDialog { Filter = "Bitmap Image|*.bmp" };
-				if (save.ShowDialog() == DialogResult.OK)
-				{
-					var bmp = new BitmapData(2, 2);
-					bmp.Save(save.FileName);
-				}
+			var save = new SaveFileDialog { Filter = "Bitmap Image|*.bmp" };
+			if (save.ShowDialog() == DialogResult.OK)
+			{
+				var bmp = new BitmapData(7, 7);
+				bmp.Data[0, 0] = new BitmapData.Pixel { R = 255, G = 0, B = 0 };
+				bmp.Data[0, 6] = new BitmapData.Pixel { R = 128, G = 0, B = 128 };
+				bmp.Data[6, 0] = new BitmapData.Pixel { R = 0, G = 64, B = 0 };
+				bmp.Data[6, 6] = new BitmapData.Pixel { R = 0, G = 0, B = 32 };
+				bmp.Save(save.FileName);
+			}
 			//}
 		}
 
